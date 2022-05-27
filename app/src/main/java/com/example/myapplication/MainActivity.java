7e6758fb59d3354a7aca5eba433c1fb974d4e581
littleak <littleak@student.21-school.ru>
2022-05-27 13:27:18 +0500

    Revision

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,18 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         databaseHelper = new DB(getApplicationContext());
         db = databaseHelper.getReadableDatabase();
-        Cursor coutCursor =  db.rawQuery("select * from "+ DB.TABLE, null);
-        Cout = coutCursor.getCount();
-        if(Cout == 2) {
+        Cursor CoutCursor =  db.rawQuery("select * from "+ DB.TABLE, null);
+        Cout = CoutCursor.getCount();
+        if(Cout == 3) {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "Привет ты запустил меня 3 раза)!", Toast.LENGTH_SHORT);
             toast.show();
-        } else {
-            ContentValues contentValues = new ContentValues();
-            contentValues.put("number", Cout);
-            db.insert("Task", null, contentValues);
         }
-
     }
     @Override
     public void onBackPressed() {
